3170ac1a5d58420c5fa33110cbeb41e1217564d8
Pål Blomseth Søresen <sorpal12@student.westerdals.no>
2015-05-01 12:46:19 +0200

    sada

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -16,7 +16,7 @@ set :rails_env, :production
 # definition into the server list. The second argument
 # something that quacks like a has can be used to set
 # extended properties on the server.
-server '192.241.188.190', user: 'deploy', roles: %w{web app}
+server '191.239.218.126', user: 'deploy', roles: %w{web app}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
